ab56718d5021c42e3778e37a152d110612575567
Andrew Mbiam <andrew.mbiam@gmail.com>
2019-03-03 15:22:29 +0000

    Rendering Posts from Left to right

--- templates/posts.php
+++ templates/posts.php
@@ -14,11 +14,12 @@
 		'number' => '',
 		'offset' => 0,
 		'post_type' => 'post',
-		'post_status' => 'publish'
+		'post_status' => 'publish',
+		'posts_per_page' => 30
 	);
 
 	/*ps: per section*/
-	$posts = get_posts();
+	$posts = get_posts($args);
 	$posts_ps = ceil(count($posts)/3); 
 
 ?>
@@ -30,37 +31,38 @@
 	</div>
 	<?php $count = 0; ?>
 	<div class="row">
-		<!-- Opening div for Pages -->
-		<div class="col-md-4 body-text">
+		<!-- Opening div for Posts -->
+		<!--<div class="col-md-4 body-text">-->
 		<?php foreach($posts as $post): ?>
 			<?php if($posts_ps == $count ||  $count == ($posts_ps*2))  : ?>
-				<!-- Create New Section -->
+				<!-- Create New Section 
 				</div>
-				<div class="col-md-4 body-text">
+				<div class="col-md-4 body-text">-->
 			<?php endif; ?>
 
 			<?php
 				$thumb = get_post_thumbnail_id( $post->ID );
 	  			$image = wp_get_attachment_image_src($thumb,'single-post-thumbnail'); 
 	  		?>
-
-			<div style="background-image: url(<?= $image[0] ?>)" class="bg-paralax ng-bg-4">
-				<div class="full-width top-margin-small top-padding-lg bottom-padding-lg ng-bg-2 bg-cover">
-					<div class="ng-bg-white three-quarter-width left-margin-md">
-						<a href="<?php echo get_page_link($post->ID)?>" class="page-link left-padding-sm">
-						    <?php echo $post->post_title; ?>
-						</a>
-					</div>
-					<div class="ng-bg-4 ng-color-black three-quarter-width left-margin-md left-padding-sm">
-						<?php echo date("M d, Y", strtotime($post->post_date)); ?>
+	  		<div class="col-sm-4 body-text">
+	  			<!-- Start Styling for Post Object -->
+				<div style="background-image: url(<?= $image[0] ?>)" class="bg-paralax ng-bg-4">
+					<div class="full-width top-margin-small top-padding-lg bottom-padding-lg ng-bg-2 bg-cover">
+						<div class="ng-bg-white three-quarter-width left-margin-md">
+							<a href="<?php echo get_page_link($post->ID)?>" class="page-link left-padding-sm">
+							    <?php echo $post->post_title; ?>
+							</a>
+						</div>
+						<div class="ng-bg-4 ng-color-black three-quarter-width left-margin-md left-padding-sm">
+							<?php echo date("M d, Y", strtotime($post->post_date)); ?>
+						</div>
 					</div>
 				</div>
+				<!-- End Styling for Post Object -->
 			</div>
-
-
 			<?php $count++; ?>
 		<?php endforeach; ?>
-		</div> 
-		<!-- Closing div for Pages -->
+		<!--</div>-->
+		<!-- Closing div for Posts -->
 	</div>
 </div>
